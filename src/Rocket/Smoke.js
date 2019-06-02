6a604fd269e69eadb127c09856f831293d9edc4c
KryptKode <paulakhamiogu@gmail.com>
2019-06-02 23:33:48 +0100

    Fixed path bugs

--- src/Rocket/Smoke.js
+++ src/Rocket/Smoke.js
@@ -5,7 +5,7 @@ export default class Smoke extends React.Component {
   render() {
     return (
       <div className={styles.smoke}>
-        <video className={styles.video} autoPlay loop src='/smoke.mov' />
+        <video className={styles.video} autoPlay loop src='smoke.mov' />
       </div>
     );
   }
