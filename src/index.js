39e5a55b26b8aa3a677c2663f6a3bcdeb26749e9
KryptKode <paulakhamiogu@gmail.com>
2019-06-02 09:07:12 +0100

    Added styles to background component using composes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
