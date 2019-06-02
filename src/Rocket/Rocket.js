348bd29924546f1140fbb1cc617cb730fb716d03
KryptKode <paulakhamiogu@gmail.com>
2019-06-02 20:51:04 +0100

    Added final items

--- src/Rocket/Rocket.js
+++ src/Rocket/Rocket.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import styles from './Rocket.module.css';
 
 export default class Rocket extends React.Component {
   render() {
     return (
       <div>
-        <h1>Rocket</h1>
+        <img 
+        src='/shuttle.png'
+        alt='shuttle'
+        className={styles.img} />
       </div>
     );
   }
